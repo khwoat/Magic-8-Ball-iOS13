19df7650ee00056a1af5b16bda019a051d777bc7
THIWAKORN <thiwakorn.sri@bualuang.co.th>
2024-02-29 11:43:06 +0700

    Complete App

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
+    @IBOutlet weak var ballImageView: UIImageView!
+    
+    let ballArray = [UIImage(named: "ball1"), UIImage(named: "ball2"), UIImage(named: "ball3"), UIImage(named: "ball4"), UIImage(named: "ball5")]
+    
+    
+    @IBAction func onAskButtonPressed(_ sender: UIButton) {
+        ballImageView.image = ballArray.randomElement() ?? ballArray[0]
+    }
+    
 
 
 }
